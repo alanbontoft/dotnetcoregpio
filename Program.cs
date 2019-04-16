dc2bd8a4e8451d6ada3bee096185aeca6abe54a2
alanbontoft <alan.bontoft@gmail.com>
2019-04-16 12:17:11 +0100

    SPI working on GertBoard DAC

--- Program.cs
+++ Program.cs
@@ -12,22 +12,23 @@ namespace gpiotest
         static void Main(string[] args)
         {
             var pin = 17;
-            var lightTimeInMilliseconds = 250;
-            var dimTimeInMilliseconds = 250;
-            byte data = 1;
+            var lightTimeInMilliseconds = 500;
+            var dimTimeInMilliseconds = 500;
+            byte[] buffer = new byte[2];
+            UInt16 data = 0, config, sendValue;
 
-            // Serial Port
             System.IO.Ports.SerialPort port = new SerialPort();
-            
+
             // SPI
-            // using '/dev/spidev0.0' (use CS = 1 for spidev0.1)
+            // DAC on GertBoard on CS1
+            // using '/dev/spidev0.1' (use chipSelect = 0 for spidev0.0)
             var busId = 0;
-            var chipSelect = 0;
+            var chipSelect = 1;
             var settings = new SpiConnectionSettings(busId, chipSelect);
 
+            // create device to talk to DtoA (MCP4802)
             SpiDevice spi = new UnixSpiDevice(settings);
-            
-            Console.WriteLine($"Let's blink an LED!");
+
             using (GpioController controller = new GpioController())
             {
                 controller.OpenPin(pin, PinMode.Output);
@@ -35,7 +36,6 @@ namespace gpiotest
 
                 Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
                 {
-                    
                     controller.Dispose();
                 };
 
@@ -48,11 +48,33 @@ namespace gpiotest
                     controller.Write(pin, PinValue.Low);
                     Thread.Sleep(dimTimeInMilliseconds);
                     
-                    // send byte via SPI and increment value
-                    spi.WriteByte(data++);
+                    // data has to be sent as 16 bit value with CS low for both bytes
+                    // 
+                    config = 0x3000;    // Channel A, Gain = 1 (0011)
+                    // config = 0xB000;    // Channel B, Gain = 1 (1011)
+                    // config = 0x1000;    // Channel A, Gain = 2 (0001)
+                    // config = 0x9000;    // Channel B, Gain = 2 (1001)
+
+                    // data left shifted by 4 bits
+                    sendValue = (ushort)(data << 4);
+                    sendValue |= config;
+
+                    buffer[0] = (byte)((sendValue & 0xFF00) >> 8);
+                    buffer[1] = (byte)(sendValue & 0x00FF);
+                    
+                    // send using Write(ReadOnlySpan) as WriteByte will raise CS between bytes 
+                    ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(buffer);
+                    
+                    // write 16 bit value to DAC
+                    spi.Write(span);
+
+                    // increment data value
+                    data += 10;
+
+                    // reset data value
+                    if (data >= 256) data = 0;
                 }
             }
-
         }
     }
 }
